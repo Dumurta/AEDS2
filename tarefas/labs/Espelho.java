47e3d99ce53306008f9af2cf740139f00919bec9
Eduardo Murta De Abreu <eduardomurtaabreu@gmail.com>
2025-09-14 19:24:54 -0300

    inicio exercicio lab 02 Espelho

---
+++ tarefas/labs/Espelho.java
@@ -0,0 +1,15 @@
+package AEDS2.tarefas.labs;
+
+public class Espelho {
+    public static int reverter(int[] array)
+    {
+        int s2;
+        int tamanho = array.length;
+        for(int i = tamanho; i >= tamanho; i--)
+        {
+            int j = 0;
+            s2[j] += array[i];
+            j++;
+        }
+    }
+}
