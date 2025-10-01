6456e7a47a17a1c53f154332af85f94b4cee5776
Eduardo Murta De Abreu <158529465+Dumurta@users.noreply.github.com>
2025-10-01 14:06:50 -0300

    Começo do tp4

---
+++ tarefas/tp04/AnalisarJogos.java
@@ -0,0 +1,11 @@
+package tarefas.tp04;
+
+
+public class AnalisarJogos {
+    static class Jogos{
+        private int appID, donos, score, userScore, achievementes;
+        private String name, releaseDate, publisher, dev, languages, categories, genres, tags;
+        private float preco;
+
+    }
+}
