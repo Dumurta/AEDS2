336f8b5e3135ecba79d8340910715a9bbe4bd300
Eduardo Murta <eduardomurtaabreu@gmail.com>
2025-10-03 19:54:34 -0300

    Tp04

--- tarefas/tp04/AnalisarJogos.java
+++ tarefas/tp04/AnalisarJogos.java
@@ -1,25 +1,28 @@
-import java.io.*;
-
 public class AnalisarJogos {
     static class Jogos{
-        private int appID, donos, score, userScore, achievementes;
-        private String name, releaseDate, publisher, dev, languages, categories, genres, tags;
-        private float preco;
+        private int id, donos, m_score, u_score, conquistas;
+        private String name, pubs, devs;
+        private String[] data, lang, categories, genres, tags;
 
-        public static void lerJogos(){
-            String file = "tmp\\games.csv";
-            BufferedReader bf = new BufferedReader(new FileReader(file));
-            String[] linha = {};
-            
-            while(){
-
-            }
-
-
-
-
+        public Jogos(int id, int donos, int m_score, int u_score, int conquistas, 
+        String name, String pubs, String devs, String[] data, String[] lang, 
+        String[] categories, String[] tags, String[] genres)
+        {
+                this.id = id;
+                this.donos = donos;
+                this.m_score = m_score;
+                this.u_score = u_score;
+                this.conquistas = conquistas;
+                this.name = name;
+                this.pubs = pubs;
+                this.devs = devs;
+                this.data = data;
+                this.lang = lang;
+                this.categories = categories;
+                this.genres = genres;
+                this.tags = tags;
         }
+
+        
     }
-
-
 }
